3a6796a5a2811cf4d15ceb664257425f521df8e1
macarron001 <rpinoon16@gmail.com>
2022-08-17 23:44:42 +0800

    added book relation and validation

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,8 @@
 class Book < ApplicationRecord
+  has_many :list_items
+  has_many :users, through: :list_items
+
+  validates :title, presence: true
+  validates :author, presence: true
+  validates :publisher, presence: true
 end
