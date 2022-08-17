4fd1719783c5d370e6d756e1d50c5fcb42d1c8f1
macarron001 <rpinoon16@gmail.com>
2022-08-17 23:46:48 +0800

    configured factorybot for list_item

--- spec/factories/list_items.rb
+++ spec/factories/list_items.rb
@@ -1,6 +1,10 @@
 FactoryBot.define do
   factory :list_item do
-    user { nil }
-    book { nil }
+    user
+    book
+    rating { -1 }
+    notes { "sample note" }
+    start_date { "2022-08-17 17:59:58" }
+    finish_date { "2022-08-17 17:59:58" }
   end
 end
