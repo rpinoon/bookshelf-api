8e424c37c205f87db4d073178765e5c354c86acd
macarron001 <rpinoon16@gmail.com>
2022-08-17 22:46:15 +0800

    added jti revocation

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,6 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :authentication_keys => [:username]
+         :recoverable, :rememberable, :jwt_authenticatable, jwt_revocation_strategy: self, :authentication_keys => [:username]
+         
 end
