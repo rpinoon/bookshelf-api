d117336ea090cc3e1816538ebcd6133cda6b34ee
macarron001 <rpinoon16@gmail.com>
2022-08-17 23:50:08 +0800

    added relation and validation to list_item model

--- app/models/list_item.rb
+++ app/models/list_item.rb
@@ -1,4 +1,8 @@
 class ListItem < ApplicationRecord
   belongs_to :user
   belongs_to :book
+
+  validates :user_id, presence: true
+  validates :book_id, uniqueness: true
+  validates :rating, presence: true, inclusion: { in: [-1, 1, 2, 3, 4, 5] }
 end
