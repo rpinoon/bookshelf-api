b1f9cbc6476ec4a65f08aa249e2e297eae1acd1e
macarron001 <rpinoon16@gmail.com>
2022-08-17 23:33:53 +0800

    added relation and validations to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,12 @@
 class User < ApplicationRecord
   include Devise::JWT::RevocationStrategies::JTIMatcher
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :jwt_authenticatable, jwt_revocation_strategy: self, :authentication_keys => [:username]
-         
+  
+  has_many :list_items
+  has_many :books, through: :list_items
+  
+  validates :username, presence: true, uniqueness: true
+  validates :password, presence: true, length: {minimum: 6}
 end
