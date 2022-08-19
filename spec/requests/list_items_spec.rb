c6b6aeca7b146b1b142fb49f65bd0122740b5c30
macarron001 <rpinoon16@gmail.com>
2022-08-19 17:30:00 +0800

    fixed syntax and typo errors in list_item test

--- spec/requests/list_items_spec.rb
+++ spec/requests/list_items_spec.rb
@@ -12,7 +12,7 @@ RSpec.describe "ListItems", type: :request do
   end
 
   it 'returns all list items' do
-    get '/list'
+    get '/list_items'
     json = JSON.parse(response.body)
 
     expect(response.content_type).to eq("application/json; charset=utf-8")
@@ -20,56 +20,68 @@ RSpec.describe "ListItems", type: :request do
     expect(json.length).to eq(3)
   end
 
-  it 'returns a specific book' do
-    get '/list_item', params: { id: @list3.id }
+  it 'returns a specific list_item' do
+    get "/list_items/#{@list3.id}"
     json = JSON.parse(response.body)
 
     expect(response.content_type).to eq("application/json; charset=utf-8")
     expect(response).to have_http_status(:success)
-    expect(json['start_date']).to eq(@list3.start_date)
+    expect(Date.parse(json['start_date'])).to eq(@list3.start_date)
     expect(json['rating']).to eq(@list3.rating)
   end
 
   it 'creates a list item' do
-    post '/list_item', params: { list_item: { 
+    post '/list_items', params: { list_item: { 
       user_id: @user.id,
       book_id: @book.id
      } }
 
     json = JSON.parse(response.body)
+
     expect(json["status"]).to eq(201)
     expect(json["message"]).to eq('Successfully created!')
   end
 
   it 'removes a list item' do
-    delete '/list_item', params: { list_item_id: @list1.id }
+    delete "/list_items/#{@list1.id}"
 
     json = JSON.parse(response.body)
-    expect(json["status"]).to eq(201)
+    expect(json["status"]).to eq(200)
     expect(json["message"]).to eq('Successfully removed!')
   end
 
   it 'marks a list item as read' do
-    patch '/list_item', params: { list_item: { finish_date: Time.now } }
+    patch "/list_items/#{@list2.id}", params: { list_item: { finish_date: Time.now } }
 
     json = JSON.parse(response.body)
-    expect(json["status"]).to eq(201)
-    expect(json["message"]).to eq('Marked as read!')
+    expect(json["status"]).to eq(200)
+    expect(json["message"]).to eq('Successfully updated!')
   end
 
   it 'marks a list item as unread' do
-    patch '/list_item', params: { list_item: { finish_date: nil } }
+    patch "/list_items/#{@list2.id}", params: { list_item: { finish_date: nil } }
 
     json = JSON.parse(response.body)
-    expect(json["status"]).to eq(201)
-    expect(json["message"]).to eq('Marked as unread!')
+    expect(json["status"]).to eq(200)
+    expect(json["message"]).to eq('Successfully updated!')
   end
 
   it 'updates the rating of a list item' do
-    patch '/list_item', params: { list_item: { rating: 2 } }
+    patch "/list_items/#{@list1.id}", params: { list_item: { rating: 2 } }
 
     json = JSON.parse(response.body)
-    expect(json["status"]).to eq(201)
-    expect(json["message"]).to eq('Updated rating!')
+    
+    expect(json["status"]).to eq(200)
+    expect(json["message"]).to eq('Successfully updated!')
+    expect(json['list_item']['rating']).to eq(2)
+  end
+
+  it 'updates the notes of a list item' do
+    patch "/list_items/#{@list1.id}", params: { list_item: { notes: "This is a good read" } }
+
+    json = JSON.parse(response.body)
+    expect(json["status"]).to eq(200)
+    expect(json["message"]).to eq('Successfully updated!')
+    expect(json['list_item']['notes']).to eq("This is a good read")
   end
 end
