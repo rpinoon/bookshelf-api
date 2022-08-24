1a9781d1998ba34df3e77a3127a882d40f7e03db
macarron001 <rpinoon16@gmail.com>
2022-08-25 01:36:59 +0800

    added scopes for custom queries

--- app/models/user_book.rb
+++ app/models/user_book.rb
@@ -5,4 +5,7 @@ class UserBook < ApplicationRecord
   validates :user_id, presence: true
   validates :book_id, presence: true
   validates :rating, presence: true, inclusion: 0..5
+
+  scope :to_read, ->(user) { Book.where(id: where(finish_date: nil, user_id: user).pluck(:book_id)) }
+  scope :finished, ->(user) { Book.where(id: where.not(finish_date: nil, user_id: user).pluck(:book_id))}
 end
