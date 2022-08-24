9f88645e62badfc966fe7bae0508025c9b7c267a
macarron001 <rpinoon16@gmail.com>
2022-08-25 03:29:11 +0800

    placed routes inside api namespace

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
-  get '/current_user', to: 'user#index'
-  get '/books', to: 'books#index'
-  get '/book', to: 'books#show'
-  get '/discover', to: 'books#discover'
-
   namespace :api do
     resources :user_books
+
+    get '/current_user', to: 'user#index'
+    get '/books', to: 'books#index'
+    get '/book', to: 'books#show'
+    get '/discover', to: 'books#discover'
   end
 
   devise_for :users, path: '', path_names: {
