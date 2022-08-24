7ecefbc67c37ebafc2a70de932b6ba83ce781000
macarron001 <rpinoon16@gmail.com>
2022-08-25 01:38:31 +0800

    refactored user books controller

--- app/controllers/api/user_books_controller.rb
+++ app/controllers/api/user_books_controller.rb
@@ -1,12 +1,14 @@
 class Api::UserBooksController < ApplicationController
   before_action :authenticate_user!
   before_action :get_user_book, only: [:destroy, :update, :show]
-  before_action :get_reading_list, only: [:to_read]
-  before_action :get_finished_list, only: [:finished]
 
   def index
-    user_books = UserBook.all
-    render json: user_books
+    books = params[:finish_date] == nil ? UserBook.to_read(current_user) : UserBook.finished(current_user)
+    if books.empty?
+      render json: "List is empty", status: unprocessable_entity
+    else
+      render json: books
+    end
   end
 
   def show
@@ -51,44 +53,6 @@ class Api::UserBooksController < ApplicationController
       render json: @user_book.errors.full_messages, status: :unprocessable_entity
     end
   end
-
-  def to_read
-    if @reading_list.empty?
-      render json: {
-        status: 404,
-        message: 'The reading list is still empty'
-      }
-    else
-      id_array = @reading_list.pluck(:book_id)
-      books = Book.where(id: id_array)
-
-      render json: {
-        status: 200,
-        user: current_user,
-        list: @reading_list,
-        books: books
-      }
-    end 
-  end
-
-  def finished
-    if @finished_list.empty?
-      render json: {
-        status: 404,
-        message: "You haven't finished any books yet"
-      }
-    else
-      id_array = @finished_list.pluck(:book_id)
-      books = Book.where(id: id_array)
-
-      render json: {
-        status: 200,
-        user: current_user,
-        list: @finished_list,
-        books: books
-      }
-    end
-  end
   
   private
   
@@ -98,13 +62,5 @@ class Api::UserBooksController < ApplicationController
 
   def user_book_params 
     params.require(:user_book).permit(:user_id, :book_id, :rating, :notes, :start_date, :finish_date)
-  end
-
-  def get_reading_list
-    @reading_list = current_user.user_books.where(finish_date: nil)
-  end
-
-  def get_finished_list
-    @finished_list = current_user.user_books.where.not(finish_date: nil)
   end
 end
