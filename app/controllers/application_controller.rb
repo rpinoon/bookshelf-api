01dad1398214b918da01af4d2142f89087092361
macarron001 <rpinoon16@gmail.com>
2022-08-24 23:42:48 +0800

    removed rescue to avoid unreadable backend errors

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,3 @@
 class ApplicationController < ActionController::API
-  rescue_from StandardError, with: :display_error
 
-  private
-
-  def display_error(e)
-    render json: { error: e }, status: :unprocessable_entity
-  end
 end
