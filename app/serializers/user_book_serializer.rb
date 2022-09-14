55092c3a67a8a68287761379009b47b7690913e0
macarron001 <rpinoon16@gmail.com>
2022-09-14 14:45:13 +0800

    implemented 'delegate' to access attributes from book model

--- app/serializers/user_book_serializer.rb
+++ app/serializers/user_book_serializer.rb
@@ -1,5 +1,16 @@
 class UserBookSerializer
   include FastJsonapi::ObjectSerializer
-  attributes :id, :user_id, :book_id, :rating, :notes, :start_date, :finish_date
+  attributes :id, 
+              :user_id, 
+              :book_id, 
+              :rating, 
+              :notes, 
+              :start_date, 
+              :finish_date, 
+              :author, 
+              :publisher, 
+              :cover_image_url, 
+              :title,
+              :synopsis
   belongs_to :book
 end
